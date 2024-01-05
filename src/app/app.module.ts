f57b8caa05a4efa827a3bf6de09ada36b683365e
levimartines <levi.mferreira@gmail.com>
2024-01-04 22:06:55 -0300

    Bump angular/material version, fix authentication

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import {AppComponent} from './app.component';
 import {SidenavListComponent} from "./navigation/sidenav-list/sidenav-list.component";
 import {ToolbarComponent} from "./navigation/toolbar/toolbar.component";
 import {MaterialModule} from "./material/material.module";
-import {FlexLayoutModule} from "@angular/flex-layout";
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 import {PlateSearchComponent} from './components/plate-search/plate-search.component';
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
@@ -18,7 +17,18 @@ import {ServiceRegisterDialogComponent} from './components/service-register-dial
 import {AuthRoutingModule} from './auth-routing.module';
 import {registerLocaleData} from "@angular/common";
 import localePt from '@angular/common/locales/pt';
+import { OktaAuth, OktaAuthOptions } from '@okta/okta-auth-js';
+import { OktaAuthModule, OktaConfig } from '@okta/okta-angular';
 
+const oktaConfig: OktaAuthOptions = {
+  issuer: 'https://dev-10789051.okta.com/oauth2/default',
+  clientId: '0oab99obojpDufh9X5d7',
+  redirectUri: '/callback',
+  scopes: ['openid', 'profile', 'email'],
+};
+
+const oktaAuth = new OktaAuth(oktaConfig);
+const moduleConfig: OktaConfig = { oktaAuth };
 
 registerLocaleData(localePt);
 
@@ -35,12 +45,12 @@ registerLocaleData(localePt);
   ],
   imports: [
     BrowserModule,
-    FlexLayoutModule,
     MaterialModule,
     BrowserAnimationsModule,
     FormsModule,
     ReactiveFormsModule,
     HttpClientModule,
+    OktaAuthModule.forRoot(moduleConfig),
     AuthRoutingModule
   ],
   providers: [
--- src/app/auth-routing.module.ts
+++ src/app/auth-routing.module.ts
@@ -1,22 +1,13 @@
-import {CommonModule} from '@angular/common';
-import {NgModule} from '@angular/core';
-import {RouterModule, Routes} from '@angular/router';
-import {OKTA_CONFIG, OktaAuthModule, OktaCallbackComponent} from '@okta/okta-angular';
-import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
-import {OktaAuth, OktaAuthOptions} from '@okta/okta-auth-js';
-import {AuthInterceptor} from './shared/okta/auth.interceptor';
-import {PlateSearchComponent} from "./components/plate-search/plate-search.component";
-import {CarRegisterComponent} from "./components/car-register/car-register.component";
-import {CarDetailsComponent} from "./components/car-details/car-details.component";
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { OktaCallbackComponent } from '@okta/okta-angular';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { AuthInterceptor } from './shared/okta/auth.interceptor';
+import { PlateSearchComponent } from './components/plate-search/plate-search.component';
+import { CarRegisterComponent } from './components/car-register/car-register.component';
+import { CarDetailsComponent } from './components/car-details/car-details.component';
 
-const oktaConfig: OktaAuthOptions = {
-  issuer: 'https://dev-10789051.okta.com/',
-  clientId: '0oab99obojpDufh9X5d7',
-  redirectUri: '/callback',
-  scopes: ['openid', 'profile', 'groups', 'email', 'okta.roles.read'],
-};
-
-const oktaAuth = new OktaAuth(oktaConfig);
 
 const routes: Routes = [
   {
@@ -43,13 +34,12 @@ const routes: Routes = [
   imports: [
     CommonModule,
     HttpClientModule,
-    OktaAuthModule,
     RouterModule.forRoot(routes)
   ],
   providers: [
-    { provide: OKTA_CONFIG, useValue: { oktaAuth } },
-    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true }
+    {provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true}
   ],
   exports: [RouterModule]
 })
-export class AuthRoutingModule { }
+export class AuthRoutingModule {
+}
